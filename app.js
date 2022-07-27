4d9898df309e0ad501fb862261bed44d892a8bb2
remmoclock <59166652+remmoclock@users.noreply.github.com>
2022-07-27 15:52:32 +0200

    mvc

--- app.js
+++ app.js
@@ -1,12 +1,19 @@
 const mysql = require("mysql");
-var db = mysql.createConnection({
+const db = mysql.createConnection({
   host: "localhost",
   user: "root",
   password: "",
-  database: "nodejss",
+  database: "nodejs",
 });
 
 db.connect(function (err) {
   if (err) console.log("error connecting: " + err.message);
-  else console.log("state", db.state);
+  else {
+    console.log("state", db.state);
+    db.query("SELECT * FROM MEMBERS", function (err, result) {
+      if (err) {
+        console.log(err.message);
+      } else console.log(result);
+    });
+  }
 });
